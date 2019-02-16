d3edb7b8916d490dd41e556ae3c0f42b79653898
hyosunko <hyosunko@gmail.com>
2019-02-16 07:42:16 -0800

    first version of rock, paper, scissors game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,24 @@
 class GameController < ApplicationController
   def play
-    render html: "hello world, v2"
+    @human = params[:human]
+
+    rps_arr = ["rock", "paper", "scissors"]
+    human_winners = {"rock" => "scissors", "paper" => "rock", "scissors" => "paper"}
+    @computer = rps_arr[rand(0..2)]
+
+    if !rps_arr.include?(@human)
+			@result = "You entered wrong word. Please enter again"
+		else
+			if @human == @computer
+				@result = "Tied"
+			elsif human_winners[@human] == @computer
+				@result ="You won"
+			else
+				@result ="You lost"
+			end
+		end
+
+    # @result_string = "You were #{@human}, and Computer was #{computer}. #{result}"
+    render 'play.html.erb'
   end
 end
