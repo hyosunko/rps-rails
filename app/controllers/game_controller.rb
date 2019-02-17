27301887e408354abd03e63e0b27e86bd48f7d22
hyosunko <hyosunko@gmail.com>
2019-02-16 22:10:27 -0800

    optimize code

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,23 +2,17 @@ class GameController < ApplicationController
   def play
     @human = params[:human]
 
-    rps_arr = ["rock", "paper", "scissors"]
+    @computer = ["rock", "paper", "scissors"].sample
     human_winners = {"rock" => "scissors", "paper" => "rock", "scissors" => "paper"}
-    @computer = rps_arr[rand(0..2)]
 
-    if !rps_arr.include?(@human)
-			@result = "You entered wrong word. Please enter again"
+		if @human == @computer
+			@result = "The game was tied"
+		elsif human_winners[@human] == @computer
+			@result ="You won the game"
 		else
-			if @human == @computer
-				@result = "The game was tied"
-			elsif human_winners[@human] == @computer
-				@result ="You won the game"
-			else
-				@result ="You lost the game"
-			end
+			@result ="You lost the game"
 		end
 
-    # @result_string = "You were #{@human}, and Computer was #{computer}. #{result}"
     render 'play.html.erb'
   end
 end
